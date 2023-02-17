8794b04573f0dddd0d8c0b051a07d8933da6020c
pritumdebnath <122291993+pritumdebnath@users.noreply.github.com>
2023-02-17 13:49:14 -0500

    new push

--- js/app.js
+++ js/app.js
@@ -1,94 +1,94 @@
 var shop = [
     {
-        title: 'Puppy',
-        image: './images/petStorePictures/dog.png',
-        description: 'Furry and Playful',
-        price: 'Buy Now: $999'
+        title: 'Dog Treats',
+        image: './images/dogtreats.png',
+        description: 'Crunchy and Delicious',
+        price: 'Buy Now: $9'
     },
     {
-        title: 'Kitten',
-        image: './images/petStorePictures/cat.png',
-        description: 'Cute and Soft',
-        price: 'Buy Now: $499'
+        title: 'Dog Harness',
+        image: './images/dogharness.png',
+        description: 'Secure and Strong',
+        price: 'Buy Now: $19'
     },
     {
-        title: 'Parakeet',
-        image: './images/petStorePictures/bird.png',
-        description: 'Pretty and Chirpy',
-        price: 'Buy Now: $199'
-    },
-    {
-        title: 'Dog Food',
-        image: './images/petStorePictures/dogfood.png',
-        description: 'Crunchy and Tasty',
+        title: 'Cat Bed',
+        image: './images/catbed.png',
+        description: 'Soft and Comfortable',
         price: 'Buy Now: $29'
     },
-    {
-        title: 'Dog Ball Toy',
-        image: './images/petStorePictures/dogball.png',
-        description: 'Colorful and Fun',
-        price: 'Buy Now: $9'
-    },
-    {
-        title: 'Dog Leash',
-        image: './images/petStorePictures/dogleash.png',
-        description: 'Strong and Good Quality',
-        price: 'Buy Now: $19'
-    },
-    {
-        title: 'Dog Crate',
-        image: './images/petStorePictures/dogcrate.png',
-        description: 'Light and Sturdy',
-        price: 'Buy Now: $49'
-    },
-    {
-        title: 'Dog Poop Bags',
-        image: './images/petStorePictures/dogpoopbag.png',
-        description: 'Great Quality and Scented',
-        price: 'Buy Now: $4'
-    },
-    {
-        title: 'Cat Food',
-        image: './images/petStorePictures/catfood.png',
-        description: 'Fresh and Delicious',
-        price: 'Buy Now: $24'
-    },
-    {
-        title: 'Cat Collar',
-        image: './images/petStorePictures/catcollar.png',
-        description: 'Pink and Jingles',
-        price: 'Buy Now: $14'
-    },
-    {
-        title: 'Cat Toy',
-        image: './images/petStorePictures/cattoy.png',
-        description: 'Hours of Fun',
-        price: 'Buy Now: $9'
-    },
-    {
-        title: 'Cat Tower',
-        image: './images/petStorePictures/cattower.png',
-        description: 'Multi-Level Sturdy Perch',
-        price: 'Buy Now: $59'
-    },
-    {
-        title: 'Cat Litter',
-        image: './images/petStorePictures/catlitter.png',
-        description: 'Odor-Blocking and Affordable',
-        price: 'Buy Now: $19'
-    },
-    {
-        title: 'Bird Food',
-        image: './images/petStorePictures/birdfood.png',
-        description: 'Healthy and Hearty',
-        price: 'Buy Now: $39'
-    },
-    {
-        title: 'Bird Cage',
-        image: './images/petStorePictures/birdcage.png',
-        description: 'Beautiful and Secure',
-        price: 'Buy Now: $79'
-    },
+    // {
+    //     title: 'Dog Food',
+    //     image: './images/petStorePictures/dogfood.png',
+    //     description: 'Crunchy and Tasty',
+    //     price: 'Buy Now: $29'
+    // },
+    // {
+    //     title: 'Dog Ball Toy',
+    //     image: './images/petStorePictures/dogball.png',
+    //     description: 'Colorful and Fun',
+    //     price: 'Buy Now: $9'
+    // },
+    // {
+    //     title: 'Dog Leash',
+    //     image: './images/petStorePictures/dogleash.png',
+    //     description: 'Strong and Good Quality',
+    //     price: 'Buy Now: $19'
+    // },
+    // {
+    //     title: 'Dog Crate',
+    //     image: './images/petStorePictures/dogcrate.png',
+    //     description: 'Light and Sturdy',
+    //     price: 'Buy Now: $49'
+    // },
+    // {
+    //     title: 'Dog Poop Bags',
+    //     image: './images/petStorePictures/dogpoopbag.png',
+    //     description: 'Great Quality and Scented',
+    //     price: 'Buy Now: $4'
+    // },
+    // {
+    //     title: 'Cat Food',
+    //     image: './images/petStorePictures/catfood.png',
+    //     description: 'Fresh and Delicious',
+    //     price: 'Buy Now: $24'
+    // },
+    // {
+    //     title: 'Cat Collar',
+    //     image: './images/petStorePictures/catcollar.png',
+    //     description: 'Pink and Jingles',
+    //     price: 'Buy Now: $14'
+    // },
+    // {
+    //     title: 'Cat Toy',
+    //     image: './images/petStorePictures/cattoy.png',
+    //     description: 'Hours of Fun',
+    //     price: 'Buy Now: $9'
+    // },
+    // {
+    //     title: 'Cat Tower',
+    //     image: './images/petStorePictures/cattower.png',
+    //     description: 'Multi-Level Sturdy Perch',
+    //     price: 'Buy Now: $59'
+    // },
+    // {
+    //     title: 'Cat Litter',
+    //     image: './images/petStorePictures/catlitter.png',
+    //     description: 'Odor-Blocking and Affordable',
+    //     price: 'Buy Now: $19'
+    // },
+    // {
+    //     title: 'Bird Food',
+    //     image: './images/petStorePictures/birdfood.png',
+    //     description: 'Healthy and Hearty',
+    //     price: 'Buy Now: $39'
+    // },
+    // {
+    //     title: 'Bird Cage',
+    //     image: './images/petStorePictures/birdcage.png',
+    //     description: 'Beautiful and Secure',
+    //     price: 'Buy Now: $79'
+    // },
 ]
 
 var postHTML = ""
